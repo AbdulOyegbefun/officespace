701a7bba80b11e3fcfd70fe7fff959444683ed64
Abdulateef Oyegbefun <abdulateef.oyegbefun@utexas.edu>
2023-02-05 15:59:13 -0600

    Just completed 2 methods
    The methods are area and overlap. These methods are still subject to edits as they have not been tested yet

--- OfficeSpace.py
+++ OfficeSpace.py
@@ -18,16 +18,90 @@
 
 #  Date Last Modified:02/06/2022
 
+import sys
+import os
+os.system('cls')
+
 # Input: a rectangle which is a tuple of 4 integers (x1, y1, x2, y2)
 # Output: an integer giving the area of the rectangle
+
 def area (rect):
-  return None
+    x_distance = abs(rect[2]-rect[0])
+    y_distance = abs(rect[3]-rect[1])
+
+    rect_area = x_distance * y_distance
+    return rect_area
+
+def dotherectanglesoverlap(rect1,rect2):
+    rect1_x1=rect1[0]
+    rect1_y1=rect1[1]
+    rect1_x2=rect1[2]
+    rect1_y2=rect1[3]
+
+    rect2_x1=rect2[0]
+    rect2_y1=rect2[1]
+    rect2_x2=rect2[2]
+    rect2_y2=rect2[3]
+
+    x2s = (rect1_x2,rect2_x2)
+    min_of_max_x = min(x2s)
+
+    x1s = (rect1_x1,rect2_x1)
+    max_of_min_x = max(x1s)
+    
+    y2s = (rect1_y2,rect2_y2)
+    min_of_max_y = min(y2s)
+
+    y1s = (rect1_y1,rect2_y1)
+    max_of_min_y = max(y1s)
+    if (not (max_of_min_x < min_of_max_x)) or (not (max_of_min_y < min_of_max_y)):
+        return False
+
+
+    return True
 
 # Input: two rectangles in the form of tuples of 4 integers
 # Output: a tuple of 4 integers denoting the overlapping rectangle.
 #         return (0, 0, 0, 0) if there is no overlap
 def overlap (rect1, rect2):
-  return None
+    # assigning parts of the rectangle from the tuple to variables
+    if not dotherectanglesoverlap(rect1,rect2):
+        return (0,0,0,0)
+    rect1_x1=rect1[0]
+    rect1_y1=rect1[1]
+    rect1_x2=rect1[2]
+    rect1_y2=rect1[3]
+
+    rect2_x1=rect2[0]
+    rect2_y1=rect2[1]
+    rect2_x2=rect2[2]
+    rect2_y2=rect2[3]
+
+    # Finding max and min of x-coordinate of both rectangles
+    rect1_x_max = max(rect1_x1,rect1_x2)
+    rect1_x_min = min(rect1_x1,rect1_x2)
+
+    rect2_x_max = max(rect2_x1,rect2_x2)
+    rect2_x_min = min(rect2_x1,rect2_x2)
+    
+    # Finding the min of the maximum x-coordinates and the 
+    # the max of the minimum x-coordinates found above 
+    max_x = min(rect1_x_max,rect2_x_max)
+    min_x = max(rect1_x_min,rect2_x_min)
+
+
+    # The above is repeated for the y-coordinates
+    rect1_y_max = max(rect1_y1,rect1_y2)
+    rect1_y_min = min(rect1_y1,rect1_y2)
+
+    rect2_y_max = max(rect2_y1,rect2_y2)
+    rect2_y_min = min(rect2_y1,rect2_y2)
+    
+    max_y = min(rect1_y_max,rect2_y_max)
+    min_y = max(rect1_y_min,rect2_y_min)
+
+    # x0, x1, x2, x3 = min_x,min_y,max_x,max_y
+    return (min_x,min_y,max_x,max_y)
 # Input: bldg is a 2-D array representing the whole office space
 # Output: a single integer denoting the area of the unallocated 
 #         space in the office
@@ -57,22 +131,48 @@ def request_space (office, cubicles):
 # Output: a string denoting all test cases have passed
 def test_cases ():
   assert area ((0, 0, 1, 1)) == 1
+  assert area ((2, 3, 10, 11)) == 64
+  assert area ((7, 2, 18, 8)) == 66
+  assert area ((17, 11, 30, 24)) == 169
+  rect1=(2,3,10,11)
+  rect2=(7,2,18,8)
+  assert overlap(rect1,rect2) == (7,3,10,8)
+  rect1=(2,3,10,11)
+  rect2=(17,11,30,24)
+  assert overlap(rect1,rect2) == (0,0,0,0)
+
+
   # write your own test cases
 
   return "all test cases passed"
 
 def main():
   # read the data
+    grid_size = sys.stdin.readline().split()
+    total_area = int(grid_size[0])*int(grid_size[1])
 
+    number_of_people = sys.stdin.readline().strip()
+    number_of_people = int(number_of_people)
+    for i in range(number_of_people):
+        employee = sys.stdin.readline().strip()
+        employee = employee.split()
+        name = employee[0]
+        rectangle_coords = [int(i) for i in employee[1:]]
+        rectangle_coords=tuple(rectangle_coords)
+        #print(name)
+        #print(rectangle_coords)
   # run your test cases
-  '''
-  print (test_cases())=
-  '''
+    #rect1=(2,3,10,11)
+    #rect2=(7,2,18,8)
+    #print(overlap(rect1,rect2))
+
+    print (test_cases())
+
 
   # print the following results after computation
 
   # compute the total office space
-
+    print(f'Total {total_area}')
   # compute the total unallocated space
 
   # compute the total contested space
